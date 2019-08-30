3d7da536ecb130851726faea83af1c6e0e8a6a70
santoshSrinivasa <santoshsrinivasa94@gmail.com>
2019-08-30 11:01:03 +0530

    1 file modified, 1 deleted

--- index.js
+++ index.js
@@ -1,24 +1,48 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
-import Hello from './Hello';
-import './style.css';
 
-class App extends Component {
-  constructor() {
-    super();
-    
-  }
 
-  render() {
-    return (
-      <div>
-        <Hello/>
-        <p>
-          Start editing to see some magic happen :)
-        </p>
-      </div>
-    );
-  }
+function FancyBorder(props)
+{
+  return (<div>{props.children}</div>);
 }
 
-render(<App />, document.getElementById('root'));
+function Welcome(props)
+{
+  return(
+  <FancyBorder>
+     <h3>{props.title}</h3>
+     <p>{props.message}</p>
+     {props.children}
+  </FancyBorder>
+  );
+}
+
+class SignupDialog extends React.Component
+{
+  constructor(props)
+  {
+    super(props);
+    this.state={login:''};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleChange(e)
+  {
+    this.setState({login: e.target.value});
+  }
+  handleClick()
+  {
+    alert(`Welcome aboard, ${this.state.login}!`);
+  }
+ render()
+ {
+   return(
+       <Welcome title = "Mars program" message = "welcome aboard">
+       <input type="text" onChange={this.handleChange}/>
+       <button type="button" onClick={this.handleClick}>sign me up</button>
+       </Welcome>
+   );
+ }
+}
+render(<SignupDialog/>,document.getElementById('root'));
